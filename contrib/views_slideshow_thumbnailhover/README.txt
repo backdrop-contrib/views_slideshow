e9583d8c88a4d67a8ef804bf53ee08d2d90ba16e
aaronwinborn <aaron@aaronwinborn.com>
2009-04-20 22:51:08 +0000

    begin views_slideshow_thumbnailhover module to put basic functionality into /contrib

---
+++ contrib/views_slideshow_thumbnailhover/README.txt
@@ -0,0 +1,12 @@
+// $Id$
+
+Views Slideshow: Thumbnail Hover
+
+The original alternate slideshow mode for Views Slideshow.
+
+ *  The Views Slideshow: ThumbnailHover works with node views only.
+ *  It adds two parts to the view: the first part is a series of items
+ *  displayed as a list, while the second part is either full or teaser.
+ *  The slide show will synchronize the two, so that the 'active' item
+ *  will correspond to the single full/teaser item. The slide show can
+ *  be set to move automatically and/or on mouse hover.
---
+++ contrib/views_slideshow_thumbnailhover/views-slideshow-thumbnailhover.tpl.php
@@ -0,0 +1,36 @@
+<?php
+// $Id$
+
+/**
+ *  @file
+ *  Views Slideshow: Thumbnail Hover
+ */
+
+  $base = drupal_get_path('module', 'views_slideshow_thumbnailhover');
+  drupal_add_js($base . '/js/views_slideshow.js', 'module');
+  drupal_add_css($base . '/css/views_slideshow.css', 'module');
+
+  $js = theme('views_slideshow_div_js', $rows, $options, $id);
+  drupal_add_js($js, 'inline');
+
+  $teaser = ($options['hover_breakout'] == 'teaser' ? TRUE : FALSE);
+  $output = '';
+  $view_teasers = FALSE;
+
+  // As we're using the 'thumbnail hover' mode, then we need to display all the view thumbnails.
+  $view_teasers = theme('views_slideshow_breakout_teasers', $rows, $id);
+  if (!$options['teasers_last']) {
+    $output .= $view_teasers;
+  }
+
+  // These are hidden elements, used to cycle through the main div
+  $hidden_elements = theme('views_slideshow_no_display_section', $view, $rows, $id, $options['mode'], $teaser);
+  $output .= theme('views_slideshow_main_section', $id, $hidden_elements);
+
+  if ($view_teasers && $options['teasers_last']) {
+    $output .= $view_teasers;
+  }
+
+  print $output;
+
+?>
