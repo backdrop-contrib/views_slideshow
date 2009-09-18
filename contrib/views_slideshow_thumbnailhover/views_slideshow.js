a438fc3bb3009cea1ef8f15992a969810906d2fe
Adam Moore <redndahead@gmail.com>
2009-09-18 23:06:49 +0000

    Add missing file from last commit

--- contrib/views_slideshow_thumbnailhover/views_slideshow.js
+++ contrib/views_slideshow_thumbnailhover/views_slideshow.js
@@ -9,7 +9,7 @@
  *  This will set our initial behavior, by starting up each individual slideshow.
  */
 Drupal.behaviors.viewsSlideshowThumbnailHover = function (context) {
-  $('.views_slideshow_main:not(.viewsSlideshowThumbnailHover-processed)', context).addClass('viewsSlideshowThumbnailHover-processed').each(function() {
+  $('.views_slideshow_thumbnailhover_main:not(.viewsSlideshowThumbnailHover-processed)', context).addClass('viewsSlideshowThumbnailHover-processed').each(function() {
     var fullId = '#' + $(this).attr('id');
     var settings = Drupal.settings.viewsSlideshowThumbnailHover[fullId];
     settings.targetId = '#' + $(fullId + " :first").attr('id');
@@ -21,17 +21,17 @@ Drupal.behaviors.viewsSlideshowThumbnailHover = function (context) {
       pause:settings.pause==1,
       pager:(settings.pager_event == 'hoverIntent') ? null : '#views_slideshow_breakout_teasers_' + settings.id,
       pagerAnchorBuilder:(settings.pager_event == 'hoverIntent') ? null : function(idx, slide) { 
-        return '#views_slideshow_div_breakout_teaser_' + settings.id + '_' + idx; 
+        return '#views_slideshow_thumbnailhover_div_breakout_teaser_' + settings.id + '_' + idx; 
       },
       before:(settings.pager_event == 'hoverIntent') ? function(current,next) {
         var currId = (currId=$(current).attr('id')).substring(currId.lastIndexOf('_')+1)
         var nextId = (nextId=$(next).attr('id')).substring(nextId.lastIndexOf('_')+1)
-        $('#views_slideshow_div_breakout_teaser_' + settings.id + '_' + currId).removeClass('activeSlide');
-        $('#views_slideshow_div_breakout_teaser_' + settings.id + '_' + nextId).addClass('activeSlide');
+        $('#views_slideshow_thumbnailhover_div_breakout_teaser_' + settings.id + '_' + currId).removeClass('activeSlide');
+        $('#views_slideshow_thumbnailhover_div_breakout_teaser_' + settings.id + '_' + nextId).addClass('activeSlide');
       } : null,
       pagerEvent: (settings.pager_event == 'hoverIntent') ? null : settings.pager_event,
-      prev:(settings.controls > 0)?'#views_slideshow_prev_' + settings.id:null,
-      next:(settings.controls > 0)?'#views_slideshow_next_' + settings.id:null,
+      prev:(settings.controls > 0)?'#views_slideshow_thumbnailhover_prev_' + settings.id:null,
+      next:(settings.controls > 0)?'#views_slideshow_thumbnailhover_next_' + settings.id:null,
       cleartype:(settings.cleartype),
       cleartypeNoBg:(settings.cleartypenobg)
     };
@@ -46,14 +46,14 @@ Drupal.behaviors.viewsSlideshowThumbnailHover = function (context) {
     $(settings.targetId).cycle(settings.opts);
     
     if (settings.pager_event == 'hoverIntent') {
-      $('#views_slideshow_breakout_teasers_' + settings.id + ' .views_slideshow_div_breakout_teaser').each(function(i,obj) {
+      $('#views_slideshow_thumbnailhover_breakout_teasers_' + settings.id + ' .views_slideshow_thumbnailhover_div_breakout_teaser').each(function(i,obj) {
         $(obj).hoverIntent(
           function() {
-            $('.views_slideshow_div_breakout_teaser').removeClass('activeSlide');
+            $('.views_slideshow_thumbnailhover_div_breakout_teaser').removeClass('activeSlide');
             var id = $(this).attr('id');
             id = parseInt(id.substring(id.lastIndexOf('_')+1));
             $(settings.targetId).cycle(id);
-            $('#views_slideshow_div_breakout_teaser_' + settings.id + '_' + id).addClass('activeSlide');
+            $('#views_slideshow_thumbnailhover_div_breakout_teaser_' + settings.id + '_' + id).addClass('activeSlide');
             $(settings.targetId).cycle('stop');
           },
           function() {
@@ -66,15 +66,15 @@ Drupal.behaviors.viewsSlideshowThumbnailHover = function (context) {
     }
    
     if (settings.controls > 0) {
-      $('#views_slideshow_playpause_' + settings.id).click(function(e) {
+      $('#views_slideshow_thumbnailhover_playpause_' + settings.id).click(function(e) {
         if (settings.paused) {
           $(settings.targetId).cycle('resume');
-          $('#views_slideshow_playpause_' + settings.id).addClass('views_slideshow_pause').removeClass('views_slideshow_play').text('Pause');
+          $('#views_slideshow_thumbnailhover_playpause_' + settings.id).addClass('views_slideshow_thumbnailhover_pause').removeClass('views_slideshow_thumbnailhover_play').text('Pause');
           settings.paused = false;
         }
         else {
           $(settings.targetId).cycle('pause');
-          $('#views_slideshow_playpause_' + settings.id).addClass('views_slideshow_play').removeClass('views_slideshow_pause').text('Resume');
+          $('#views_slideshow_thumbnailhover_playpause_' + settings.id).addClass('views_slideshow_thumbnailhover_play').removeClass('views_slideshow_thumbnailhover_pause').text('Resume');
           settings.paused = true;
         }
         e.preventDefault();
