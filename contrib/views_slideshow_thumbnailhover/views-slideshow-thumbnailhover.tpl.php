75cd0c90703e6f012d00f908d8f9eb3b3ff20e3e
Adam Moore <redndahead@gmail.com>
2009-11-17 05:55:19 +0000

    #554438 by jcmarco, fearlsgroove, redndahead: Added Support FIELDS as views rows style.

--- contrib/views_slideshow_thumbnailhover/views-slideshow-thumbnailhover.tpl.php
+++ contrib/views_slideshow_thumbnailhover/views-slideshow-thumbnailhover.tpl.php
@@ -18,20 +18,20 @@
   $view_teasers = FALSE;
 
   // As we're using the 'thumbnail hover' mode, then we need to display all the view thumbnails.
-  $view_teasers = theme('views_slideshow_thumbnailhover_breakout_teasers', $rows, $id);
+  $view_teasers = theme('views_slideshow_thumbnailhover_breakout_teasers', $view, $rows, $id, $options);
   if (!$options['thumbnailhover']['teasers_last']) {
     $output .= $view_teasers;
   }
 
   // These are hidden elements, used to cycle through the main div
-  $hidden_elements = theme('views_slideshow_thumbnailhover_no_display_section', $view, $rows, $id, $options['mode'], $teaser);
+  $hidden_elements = theme('views_slideshow_thumbnailhover_no_display_section', $view, $rows, $id, $options, $teaser);
   $output .= theme('views_slideshow_main_section', $id, $hidden_elements, 'thumbnailhover');
 
   if ($view_teasers && $options['thumbnailhover']['teasers_last']) {
     $output .= $view_teasers;
   }
   if ($options['thumbnailhover']['controls'] == 2) {
-    print theme('views_slideshow_thumbnailhover_controls',$id,$view,$options);
+    print theme('views_slideshow_thumbnailhover_controls',$id, $view, $options);
   }
   
   if ($options['thumbnailhover']['image_count'] == 2) {
