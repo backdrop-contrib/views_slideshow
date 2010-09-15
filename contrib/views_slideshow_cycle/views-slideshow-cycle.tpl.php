85125b3714d99c3c8fd8be4df8a4bc91786b7cfe
Adam Moore <redndahead@gmail.com>
2010-09-15 09:09:48 +0000

    #851734 by redndahead: Added Add ability to define 'skins'.

--- contrib/views_slideshow_cycle/views-slideshow-cycle.tpl.php
+++ contrib/views_slideshow_cycle/views-slideshow-cycle.tpl.php
@@ -7,36 +7,38 @@
  */
 ?>
 
-<?php if ($controls_location == 'top' || $pager_location == 'top' || $slide_counter_location == 'top'): ?>
-  <div class="views-slideshow-controls-top clear-block">
-    <?php if ($slide_counter_location == 'top') { ?>
-      <?php print $slide_counter; ?>
-    <?php } ?>
-    
-    <?php if ($controls_location == 'top') { ?>
-      <?php print $controls; ?>
-    <?php } ?>
-    
-    <?php if ($pager_location == 'top') { ?>
-      <?php print $pager; ?>
-    <?php } ?>
-  </div>
-<?php endif; ?>
-
-<?php print $slideshow; ?>
-
-<?php if ($controls_location == 'bottom' || $pager_location == 'bottom' || $slide_counter_location == 'bottom'): ?>
-  <div class="views-slideshow-controls-bottom clear-block">
-    <?php if ($pager_location == 'bottom') { ?>
-      <?php print $pager; ?>
-    <?php } ?>
-    
-    <?php if ($controls_location == 'bottom') { ?>
-      <?php print $controls; ?>
-    <?php } ?>
-    
-    <?php if ($slide_counter_location == 'bottom') { ?>
-      <?php print $slide_counter; ?>
-    <?php } ?>
-  </div>
-<?php endif; ?>
+<div class="skin-<?php print $options['views_slideshow_cycle']['skin']; ?>">
+  <?php if ($controls_location == 'top' || $pager_location == 'top' || $slide_counter_location == 'top'): ?>
+    <div class="views-slideshow-controls-top clear-block">
+      <?php if ($slide_counter_location == 'top') { ?>
+        <?php print $slide_counter; ?>
+      <?php } ?>
+      
+      <?php if ($controls_location == 'top') { ?>
+        <?php print $controls; ?>
+      <?php } ?>
+      
+      <?php if ($pager_location == 'top') { ?>
+        <?php print $pager; ?>
+      <?php } ?>
+    </div>
+  <?php endif; ?>
+  
+  <?php print $slideshow; ?>
+  
+  <?php if ($controls_location == 'bottom' || $pager_location == 'bottom' || $slide_counter_location == 'bottom'): ?>
+    <div class="views-slideshow-controls-bottom clear-block">
+      <?php if ($pager_location == 'bottom') { ?>
+        <?php print $pager; ?>
+      <?php } ?>
+      
+      <?php if ($controls_location == 'bottom') { ?>
+        <?php print $controls; ?>
+      <?php } ?>
+      
+      <?php if ($slide_counter_location == 'bottom') { ?>
+        <?php print $slide_counter; ?>
+      <?php } ?>
+    </div>
+  <?php endif; ?>
+</div>
