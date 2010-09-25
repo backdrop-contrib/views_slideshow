c2239dde5862c50d551eba9df86ddb1ae0c73fc6
Adam Moore <redndahead@gmail.com>
2010-09-25 16:30:04 +0000

    #908996 by redndahead: Added Need to be able to give pager/controls/slide counter Weights.

--- contrib/views_slideshow_cycle/views-slideshow-cycle.tpl.php
+++ contrib/views_slideshow_cycle/views-slideshow-cycle.tpl.php
@@ -8,37 +8,17 @@
 ?>
 
 <div class="skin-<?php print $options['views_slideshow_cycle']['skin']; ?>">
-  <?php if ($controls_location == 'top' || $pager_location == 'top' || $slide_counter_location == 'top'): ?>
+  <?php if (isset($top_widget_rendered)): ?>
     <div class="views-slideshow-controls-top clear-block">
-      <?php if ($slide_counter_location == 'top') { ?>
-        <?php print $slide_counter; ?>
-      <?php } ?>
-      
-      <?php if ($controls_location == 'top') { ?>
-        <?php print $controls; ?>
-      <?php } ?>
-      
-      <?php if ($pager_location == 'top') { ?>
-        <?php print $pager; ?>
-      <?php } ?>
+      <?php print $top_widget_rendered; ?>
     </div>
   <?php endif; ?>
   
   <?php print $slideshow; ?>
   
-  <?php if ($controls_location == 'bottom' || $pager_location == 'bottom' || $slide_counter_location == 'bottom'): ?>
+  <?php if (isset($bottom_widget_rendered)): ?>
     <div class="views-slideshow-controls-bottom clear-block">
-      <?php if ($pager_location == 'bottom') { ?>
-        <?php print $pager; ?>
-      <?php } ?>
-      
-      <?php if ($controls_location == 'bottom') { ?>
-        <?php print $controls; ?>
-      <?php } ?>
-      
-      <?php if ($slide_counter_location == 'bottom') { ?>
-        <?php print $slide_counter; ?>
-      <?php } ?>
+      <?php print $bottom_widget_rendered; ?>
     </div>
   <?php endif; ?>
 </div>
