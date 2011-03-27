a2ffdaf0270061ccd616671e2b670f2ca66678fe
amoore5 <amoore5@ucmerced.edu>
2011-03-27 14:55:04 -0700

    Issue #1079960 by redndahead | rvilar: Fixed Advanced options that require return values do not work.

--- contrib/views_slideshow_cycle/js/views_slideshow_cycle.js
+++ contrib/views_slideshow_cycle/js/views_slideshow_cycle.js
@@ -229,6 +229,7 @@
               case "pagerAnchorBuilder":
                 // callback fn for building anchor links:  function(index, DOMelement)
                 settings.opts[option] = function(index, DOMelement) {
+                  var returnVal = '';
                   eval(advancedOptions[option]);
                   return returnVal;
                 }
